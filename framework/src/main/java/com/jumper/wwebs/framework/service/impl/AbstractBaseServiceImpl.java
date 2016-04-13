dc672d71df72687fe0e99dcc7ae3623c1be275b8
hank-wang <12497487@qq.com>
2016-04-13 16:42:38 +0800

    init

---
+++ framework/src/main/java/com/jumper/wwebs/framework/service/impl/AbstractBaseServiceImpl.java
@@ -0,0 +1,7 @@
+package com.jumper.wwebs.framework.service.impl;
+
+import com.jumper.wwebs.framework.service.AbstractBaseService;
+
+public abstract class AbstractBaseServiceImpl implements AbstractBaseService{
+
+}
